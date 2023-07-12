4dd96c3903f111a44dc39b8074f18dda00669cdf
Lee hyunkyoung <hyunk.office@gmail.com>
2023-07-12 15:56:46 +0900

    init commit

---
+++ src/main/java/com/sparta/junit5practice/JUnit5PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.junit5practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnit5PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JUnit5PracticeApplication.class, args);
+    }
+
+}
